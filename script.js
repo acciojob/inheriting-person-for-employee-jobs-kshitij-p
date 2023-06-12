b20ebdab936efd55d2fcfd1bc33b93523636446a
Kshitij P <91977631+kshitij-p@users.noreply.github.com>
2023-06-12 22:14:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+	this.greet = ()=>{
+		console.log(`Hello, my name is ${name}, I am ${age} years old.`);
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	Person.call(this, name, age);
+	this.jobTitle = jobTitle;
+
+	this.greet = ()=>{
+		console.log(`Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}.`);
+	}
+	
+}
 
 // Do not change code below this line
 window.Person = Person;
