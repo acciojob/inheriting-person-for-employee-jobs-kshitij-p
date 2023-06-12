d779b82f2e42184e100114b655985d0e071ddd4a
Kshitij P <91977631+kshitij-p@users.noreply.github.com>
2023-06-12 22:24:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,19 +2,21 @@
 function Person(name, age) {
 	this.name = name;
 	this.age = age;
-	this.greet = ()=>{
-		return `Hello, my name is ${name}, I am ${age} years old.`;
-	}
+}
+
+Person.prototype.greet = function(){
+	console.log(`Hello, my name is ${name}, I am ${age} years old.`);
 }
 
 function Employee(name, age, jobTitle) {
 	Person.call(this, name, age);
 	this.jobTitle = jobTitle;
+}
 
-	this.jobGreet = ()=>{
-		return `Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}.`;
-	}
-	
+Employee.prototype.jobGreet = function() {
+	console.log(
+	`Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}.`
+	);
 }
 
 // Do not change code below this line
